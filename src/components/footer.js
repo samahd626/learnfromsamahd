bca0886e2abc81476d888c6d26ad4bc18d2a9067
abdulsamad abdulsalam <56729488+samahd626@users.noreply.github.com>
2020-07-27 05:20:53 -0700

    added kofi and patreon

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,8 @@ const Newfooter = () => {
         <h1>Author</h1>
         <div>I'm Samahd, I write on this site to help developers like you. I don't display ads, sponsored posts and the likes on my Digital garden for max user experience. Support what I do:
         <div className="row">
-          <div className="box1">Buy me a coffee</div>
-          <div className="box2">Become a patreon</div> 
+          <a target="_blank" href="https://Ko-fi.com/samahd" style={{textDecoration: 'none', boxShadow: 'none'}}><div className="box1">Buy me a coffee</div></a>
+          <a target="_blank" href="https://patreon.com/samahd" style={{textDecoration: 'none', boxShadow: 'none'}}><div className="box2">Become a patreon</div> </a>
           </div>
         </div>
     </div>
