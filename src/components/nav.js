375d87f4b8bdaf8f13faed093452d34dbc4f3247
abdulsamad abdulsalam <56729488+samahd626@users.noreply.github.com>
2020-07-29 02:13:24 -0700

    new nav design

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,22 +7,30 @@ import '../styles/main.css'
 const Navbar = () => {
     return(
         <div className="topnav" style={{postion : 'sticky'}}>
-<Image
-        fixed="../styles/nerdexelogo.png"
-        alt="logo"
-        style={{
-          
-        }}
-        imgStyle={{
-          
-        }}
-      />
+             <Image
+        
+        fixed= {{ regex: "/nerdexelogo.png/" }}
+        
+                alt="logo"
+                style={{
+                  
+                  width: 50,
+                  height: 50,
+                  borderRadius: `10px`,
+                }}
+                imgStyle={{
+                  borderRadius: `10px`,
+                  width: 50,
+                  height: 50,
+                }}
+              />
             <Link to="/">NerdExe</Link>
-             <div className='float-right'>
-            <Link to="../pages/404">Articles</Link>
-            <Link to="../pages/404">About Me</Link>
-            <Link to="../pages/404">Contact</Link>          
-            </div>
+             
+            <Link to="../pages/404" className='float-right'>Articles</Link>
+            <Link to="../pages/404" className='float-right'>Contact me</Link>   
+            <Link to="../pages/404" className='float-right'>About Me</Link>
+
+            
         </div>
     )
 }
