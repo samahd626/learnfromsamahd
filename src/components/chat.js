ad8cd49a43564009ab4ba817ef45a0fd5e8a36e1
abdulsamad abdulsalam <56729488+samahd626@users.noreply.github.com>
2020-07-29 06:03:08 -0700

    added chat head

---
+++ src/components/chat.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from "react"
+
+
+
+const facebookAppId = "105714164327614"
+export function CustomerChat(){
+  useEffect(()=>{
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        xfbml            : true,
+        version          : 'v7.0'
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  })
+  return(
+    <>
+      <div id="fb-root" />
+      <div
+        className="fb-customerchat"
+        attribution="setup_tool"
+        page_id={facebookAppId}
+      />
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link, graphql } from "gatsby"
 
 
@@ -8,6 +8,35 @@ import Navbar from "../components/nav"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+
+const facebookAppId = "105714164327614"
+export function CustomerChat(){
+  useEffect(()=>{
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        xfbml            : true,
+        version          : 'v7.0'
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  })
+  return(
+    <>
+      <div id="fb-root" />
+      <div
+        className="fb-customerchat"
+        attribution="setup_tool"
+        page_id={facebookAppId}
+      />
+    </>
+  )
+}
 
 
 const BlogIndex = ({ data, location }) => {
